f74d82823f04b239710622fe0752aaa879a8e14b
lALTAIRl <43581932+lALTAIRl@users.noreply.github.com>
2018-12-29 12:31:38 +0300

    default news added

---
+++ Task2/Data/Migrations/20181229061339_IsPublished.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Task2.Data.Migrations
+{
+    public partial class IsPublished : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfPublishing",
+                table: "NewsCollection",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "NewsCollection",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfPublishing",
+                table: "NewsCollection");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "NewsCollection");
+        }
+    }
+}
